6063e18080c84ebe877febb8444c3f904aa312d0
southeo <soulaine.theo@gmail.com>
2023-04-17 12:27:01 +0200

    Remove unused properties

--- src/main/java/eu/dissco/orchestration/backend/security/JwtAuthConverter.java
+++ src/main/java/eu/dissco/orchestration/backend/security/JwtAuthConverter.java
@@ -1,16 +1,13 @@
 package eu.dissco.orchestration.backend.security;
 
 import java.util.Collection;
-import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimNames;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
@@ -22,17 +19,10 @@ public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationTo
 
   private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
 
-  private final JwtAuthConverterProperties properties;
-
-  public JwtAuthConverter(JwtAuthConverterProperties properties) {
-    this.properties = properties;
-  }
-
   @Override
   public AbstractAuthenticationToken convert(@NotNull Jwt jwt) {
-    Collection<GrantedAuthority> authorities = Stream.concat(
-        converterToStream(jwt),
-        extractResourceRoles(jwt).stream()).collect(Collectors.toSet());
+    Collection<GrantedAuthority> authorities =
+        converterToStream(jwt).collect(Collectors.toSet());
     return new JwtAuthenticationToken(jwt, authorities, getPrincipalClaimName(jwt));
   }
 
@@ -42,25 +32,7 @@ public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationTo
   }
 
   private String getPrincipalClaimName(Jwt jwt) {
-    String claimName = JwtClaimNames.SUB;
-    if (properties.getPrincipalAttribute() != null) {
-      claimName = properties.getPrincipalAttribute();
-    }
-    return jwt.getClaim(claimName);
-  }
-
-  private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
-    Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
-    Map<String, Object> resource;
-    Collection<String> resourceRoles;
-    if (resourceAccess == null
-        || (resource = (Map<String, Object>) resourceAccess.get(properties.getResourceId())) == null
-        || (resourceRoles = (Collection<String>) resource.get("roles")) == null) {
-      return Set.of();
-    }
-    return resourceRoles.stream()
-        .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
-        .collect(Collectors.toSet());
+    return jwt.getClaim(JwtClaimNames.SUB);
   }
 
 
