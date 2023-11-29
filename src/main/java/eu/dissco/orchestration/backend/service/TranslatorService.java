0c666c15b1a61171ccff14b0f580b714a4dc068f
Sam Leeflang <sam.leeflang@naturalis.nl>
2023-11-29 13:16:55 +0100

    Fix for k8s lib update

--- src/main/java/eu/dissco/orchestration/backend/service/TranslatorService.java
+++ src/main/java/eu/dissco/orchestration/backend/service/TranslatorService.java
@@ -126,12 +126,12 @@ public class TranslatorService {
 
   public List<TranslatorResponse> getAll() throws ApiException {
     return batchV1Api.listNamespacedJob(NAMESPACE, null, null, null, null, null, null, null, null,
-        null, null).getItems().stream().map(this::mapToResponse).toList();
+        null, null, null).getItems().stream().map(this::mapToResponse).toList();
   }
 
   public Optional<TranslatorResponse> get(String id) throws ApiException {
     return batchV1Api.listNamespacedJob(NAMESPACE, null, null, null, null, null, null, null, null,
-            null, null).getItems().stream().filter(Objects::nonNull)
+            null, null, null).getItems().stream().filter(Objects::nonNull)
         .filter(v1Job -> v1Job.getMetadata().getName().equals(id))
         .map(this::mapToResponse).findAny();
   }
